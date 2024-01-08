9686f7b2ddc6a377a8a0e3d64ac12d5f659f585c
Anthony Igbokwe <anthony.igbokwe@sycor.de>
2024-01-08 04:37:49 +0100

    new stuff

--- sas.ps1
+++ sas.ps1
@@ -1,3 +1,5 @@
+
+
 Connect-AzAccount -ServicePrincipal -Tenant $env:servicePrincipalTenantId -ApplicationId $env:servicePrincipalId -CertificateThumbprint $env:servicePrincipalKey -ServicePrincipal
 
 $sasToken = az storage blob generate-sas --account-name $storageAccountName --container-name $containerName --name $blobName --permissions $sasPermissions --expiry $sasExpiry --output tsv
