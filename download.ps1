bc4e5d5288f32d59706b72592f6045df237affd0
Anthony Igbokwe <anthony.igbokwe@sycor.de>
2024-01-09 08:28:48 +0100

    remmove ?

--- download.ps1
+++ download.ps1
@@ -7,9 +7,15 @@ param (
     
 )
 
+Write-Host "storageAccountName: $storageAccountName"
+Write-Host "containerName: $containerName"
+Write-Host "blobName: $blobName"
+Write-Host "blobSasToken: $blobSasToken"
 
 # Construct the SAS URL for the blob
-$sasUrl = "https://$storageAccountName.blob.core.windows.net/$containerName/$blobName$blobSasToken"
+$sasUrl = "https://$storageAccountName.blob.core.windows.net/$containerName/$blobName$($blobSasToken)"
+Write-Host "Constructed SAS URL: $sasUrl"
+
 # Download the CSV file
 Invoke-WebRequest -Uri $sasUrl -OutFile $destinationPath
 
